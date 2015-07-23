99439fea83109994a9f887ea288c1556e6aca3e0
Zavier Long <zavierlong@gmail.com>
2015-07-23 18:35:52 +1000

    Third commit. added .gitignore

---
+++ user_profile/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+# Create your models here.
+
+
+class User(AbstractBaseUser):
+    username = models.CharField('username', max_length=10, unique=True, db_index=True)
+    email = models.EmailField('email address', unique=True)
+    joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
