13b7e9457651012eb8449a6c537d1333dc8ba95f
Zavier Long <zavierlong@gmail.com>
2015-07-24 14:55:51 +1000

    Fourth commit. Added user and tweet models.

--- user_profile/models.py
+++ user_profile/models.py
@@ -4,8 +4,16 @@ from django.contrib.auth.models import AbstractBaseUser
 
 
 class User(AbstractBaseUser):
+
+    """Custom User Class"""
+
     username = models.CharField('username', max_length=10, unique=True, db_index=True)
     email = models.EmailField('email address', unique=True)
     joined = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+
+    def __unicode__(self):
+        return self.username
