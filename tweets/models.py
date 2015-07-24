c2d5cf7536b377ae9b76f285db6b69fbbf82de99
Zavier Long <zavierlong@gmail.com>
2015-07-24 15:05:20 +1000

    Fifth commit.

--- tweets/models.py
+++ tweets/models.py
@@ -9,3 +9,6 @@ class Tweet(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     country = models.CharField(max_length=30)
     is_active = models.BooleanField(True)
+
+    def __unicode__(self):
+        return self.text
