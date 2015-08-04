3712e17e5a6faabee7ae728d01879ab7fc8fbf3d
Zavier Long <zavierlong@gmail.com>
2015-08-04 12:05:08 +1000

    Eleventh commit.

--- tweets/forms.py
+++ tweets/forms.py
@@ -4,3 +4,9 @@ from django import forms
 class TweetForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea(attrs={'rows': 1, 'cols': 85}), max_length=160)
     country = forms.CharField(widget=forms.HiddenInput())
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField(label='Enter a keyword to search for')
+    widget = forms.TextInput(attrs={'size': 32, 'class': 'form-control'})
+
